0554614816b9fd7ae14ea55bd90f6bf28b0dbd19
Stefano Vozza <svozza@gmail.com>
2024-05-30 18:28:03 +0100

    cleanup lambda function

--- index.mjs
+++ index.mjs
@@ -1,8 +1,5 @@
 import {shout} from "./target/jco/node_rust_wasm_lambda.js";
 
-let msg = shout('bwak bwak bwak');
-console.log(msg);
-
 export async function handler(event) {
   console.log('EVENT', event)
   console.log('handler!!!', msg);
