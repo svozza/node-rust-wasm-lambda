0041ab904f634dbb85ba742b1a30f51aa1aef4d9
Stefano Vozza <svozza@gmail.com>
2024-05-30 18:38:17 +0100

    remove unneeded console.log statement

--- index.mjs
+++ index.mjs
@@ -2,6 +2,5 @@ import {shout} from "./target/jco/node_rust_wasm_lambda.js";
 
 export async function handler(event) {
   console.log('EVENT', event)
-  console.log('handler!!!', msg);
   return shout(event.key1);
 }
